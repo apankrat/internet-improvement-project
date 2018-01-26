2f4368b46bcbf03faf0697ab13ebbdf3faa831e4
Alex Pankratov <apankrat@users.noreply.github.com>
2018-01-26 21:56:59 +0100

    Update declutterTwitter.user.js

--- twitter/declutterTwitter.user.js
+++ twitter/declutterTwitter.user.js
@@ -6,36 +6,39 @@
 // @include        http://www.twitter.com/*
 // @include        https://twitter.com/*
 // @include        https://www.twitter.com/*
-// @require        http://code.jquery.com/jquery-1.7.1.min.js
+// @require        http://code.jquery.com/jquery-1.9.1.min.js
 // ==/UserScript==
 
 var burst = 30;
 
 function declutterTwitter()
 {
-	var divs =
+	var divs = 
 	  $("div.component[data-component-term='user_recommendations'], " +
 	    "div.component[data-component-term='trends'], " +
-	    "div.recent-followers-module, " +
-	    "div.promoted-tweet, " +
-	    "div.module.trends," +
-	    "div.module.Footer," +
+	    "div[data-component-context='generic_activity_MagicRecLikedTweetWithOthers'], " +
+	    "div.promoted-tweet, " + 
+	    "div.module.trends," + 
+	    "div.module.Footer," + 
 	    "div.promptbird," +
 	    "div.wtf-module, " +
 	    "div.site-footer, " +
+	    "div.module.trends, " + 
 	    "div#js-empty-timeline-recommendations-module-hook," +
-	    "div.ReonboardingCallout," +
-	    "li[data-item-type='who_to_follow_entry']," +
-	    "li[data-item-type='recap_entry']");
+			"div.ReonboardingCallout," +
+		  "li[data-item-type='who_to_follow_entry']," +
+		  "li[data-item-type='recap_entry']," +
+		  "div.SidebarCommonModules");
 
 	divs.hide();
-	document.oncontextmenu = null;
-
+	
 	if (divs.length) burst = 30;
 	else
 	if (burst) burst--;
+	
+	document.oncontextmenu = null;
 
 	setTimeout(declutterTwitter, burst ? 100 : 1000);
 }
 
-setTimeout(declutterTwitter, 100);
+declutterTwitter();
