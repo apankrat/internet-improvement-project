108519c013098e2cffea5d3047c7a560bd6ccbe6
Alex Pankratov <ap@swapped.ch>
2018-10-01 14:30:14 +0200

    jq-free version of minimalissimo/no-parallax

--- minimalissimo/noParallaxScrolling.user.js
+++ minimalissimo/noParallaxScrolling.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name           Declutter Twitter
+// @name           Disable Parallax Scrolling
 // @namespace      http://swapped.cc
 // @description    Removes "parallax scrolling" on two-column pages
 // @include        http://minimalissimo.com/*
 // @include        http://www.minimalissimo.com/*
 // @include        https://minimalissimo.com/*
 // @include        https://www.minimalissimo.com/*
-// @require        http://code.jquery.com/jquery-1.9.1.min.js
 // ==/UserScript==
 
-$('.size-1of2').addClass('.size-1of1');
+var c = document.getElementsByClassName('size-1of2');
+for (var i=0; i<c.length; i++) c[i].className += ' size-1of1';
